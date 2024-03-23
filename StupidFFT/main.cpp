7ffafe508d2369a717ceb7a14deb7c6d62cccf27
kobayashian <systemdisgood@gmail.com>
2024-03-24 00:26:48 +0300

    Fixed string to binary

--- StupidFFT/main.cpp
+++ StupidFFT/main.cpp
@@ -1,15 +1,44 @@
 #include <iostream>
 #include <vector>
 
-std::string string_of_binary(uint64_t input)
+uint64_t uint64_t_pow(uint64_t base, uint8_t exponent)
 {
-	char cstr[65];
-	cstr[64] = '\0';
-	for(uint64_t i = 0; i < 64; ++i)
+	if(exponent == 0) return 1;
+	else
 	{
-		
+		uint64_t result = base;
+		for(uint64_t i = 0; i < exponent - 1; ++i)
+		{
+			result *= base;
+		}
+		return result;
 	}
-	return std::string(cstr);
+}
+	
+
+std::string string_of_binary_uint64_t(uint64_t input)
+{
+	if(input == 0) return "0000000000000000000000000000000000000000000000000000000000000000";
+	else
+	{
+		char cstr[65];
+		cstr[64] = '\0';
+		for(uint64_t i = 1; i < 64; ++i)
+		{
+			uint64_t twos_pow = uint64_t_pow(2,i);
+			if((input % (twos_pow)) != 0)
+			{
+				cstr[64 - i] = '1';
+			}
+			else
+			{
+				cstr[64 - i] = '0';
+			}
+			input /= twos_pow;
+		}
+		return std::string(cstr);
+	}
+	
 }
 
 std::vector<uint64_t> bit_reverse_indexes_gen(uint64_t FFTpower)
@@ -24,15 +53,20 @@ std::vector<uint64_t> bit_reverse_indexes_gen(uint64_t FFTpower)
 			reversed_index |= ((((1<<bit_number) & (index<<bit_number))) << (63 - bit_number));
 		}
 		std::cout << reversed_index << std::endl;
-		std::cout << string_of_binary(reversed_index) << std::endl;
+		std::cout << string_of_binary_uint64_t(reversed_index) << std::endl;
 	}
 	return result;
 }
 
 int main()
 {
-	std::cout << string_of_binary(static_cast<uint64_t>(1)) << std::endl;
+	//std::cout << uint64_t_pow(2,3) << std::endl;
+	//std::cout << (123 % (uint64_t_pow(2,2))) << std::endl;
+	//std::cout << string_of_binary_uint64_t(65) << std::endl;
 	//std::cout << "hello" << std::endl;
 	//bit_reverse_indexes_gen(1);
+	bit_reverse_indexes_gen(3);
+
+	std::cout << "The End" << std::endl;
 	return 0;
 }
